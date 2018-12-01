6de98672b3c739404b06535102c7afa6f6ca58f5
zhangzg <zhaoguo.zhang@e-vada.com>
2018-12-02 00:43:53 +0800

    init

--- spring-cloud-auth/src/main/java/com/normal/auth/AuthApplication.java
+++ spring-cloud-auth/src/main/java/com/normal/auth/AuthApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAutoConfiguration
-//@EnableRedisHttpSession
 public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
