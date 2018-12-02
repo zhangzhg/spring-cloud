210ac701b00a748a7032ca6d28af5d6eddb5004a
zhangzg <zhaoguo.zhang@e-vada.com>
2018-12-02 23:12:09 +0800

    'init'

--- spring-cloud-auth/src/main/java/com/normal/auth/filter/AuthFilter.java
+++ spring-cloud-auth/src/main/java/com/normal/auth/filter/AuthFilter.java
@@ -1,26 +1,18 @@
 package com.normal.auth.filter;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
-
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.PRESERVE_HOST_HEADER_ATTRIBUTE;
 
 @Configuration
 public class AuthFilter implements GlobalFilter, Ordered {
     private Logger log = LoggerFactory.getLogger(AuthFilter.class);
-    private final String CAS_LOGIN = "http://www.baidu.com";
-    private final String CAS_SERVER = "http://www.baidu.com";
+
     @Override
     public int getOrder() {
         return 0;
