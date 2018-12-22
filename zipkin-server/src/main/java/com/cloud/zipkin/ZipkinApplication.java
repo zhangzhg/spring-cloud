a7ca53d0cfa853fe2a5c364d1d883d3b0902e848
zhangzg <zhaoguo.zhang@e-vada.com>
2018-12-22 16:57:45 +0800

    熔断， 监控页面

---
+++ zipkin-server/src/main/java/com/cloud/zipkin/ZipkinApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ZipkinApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
