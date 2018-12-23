6efdfdd9cbc5b3a0788bf00b7ca821e2384e4c27
zhangzg <zhaoguo.zhang@e-vada.com>
2018-12-23 12:37:57 +0800

    测试用例 权限

--- auth-service/src/main/java/com/cloud/controller/UserController.java
+++ auth-service/src/main/java/com/cloud/controller/UserController.java
@@ -8,6 +8,11 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
+    /**
+     * 获取登录用户
+     * feign client 获取不到这个，会报错，因为Principal没法序列化
+     * 需要改造下
+     */
     @GetMapping("/user")
     public Principal user(Principal user){
         return user;
--- order-service/src/main/java/com/cloud/controller/HelloController.java
+++ order-service/src/main/java/com/cloud/controller/HelloController.java
@@ -5,6 +5,7 @@ import com.cloud.service.IHelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,6 @@ public class HelloController {
     UserService userService;
 
     @RequestMapping("/hello")
-    //@PreAuthorize("hasAuthority('hello')")
     public String index(@RequestParam String name) {
         logger.info("request one  name is "+name);
         return helloService.hello(name);
@@ -31,4 +31,12 @@ public class HelloController {
         String name = userService.getHello("测试client");
         return name;
     }
+
+    @RequestMapping("/auth")
+    @PreAuthorize("hasAuthority('user')")
+    public String auth(@RequestParam String name) {
+        logger.info("request one  name is "+name);
+        return helloService.hello(name);
+    }
+
 }
