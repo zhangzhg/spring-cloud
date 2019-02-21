4c3332cbb20f5df945ef92cf0af7e2ffd8bc5620
zhangzg <zhaoguo.zhang@e-vada.com>
2019-02-21 21:19:54 +0800

    auth 最终版，将资源权限和单点登录放一起

--- auth-service/src/main/java/com/cloud/config/ResourceServerConfig.java
+++ auth-service/src/main/java/com/cloud/config/ResourceServerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletResponse;
 
